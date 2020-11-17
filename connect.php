233e77d24d810372b4e527e233fdf3f1cbd0f018
fandyakbar <48479320+fandyakbar@users.noreply.github.com>
2020-11-17 17:07:28 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "ec2-3-211-176-230.compute-1.amazonaws.com";
-	$user = "qpzadrcqaxaxjy";
-	$pass = "43b9366fc26c2cdde6e213b500325aa8db0d7acf4cdc1bcbb616abb6a2897e81";
+	$host = "ec2-18-210-90-1.compute-1.amazonaws.com";
+	$user = "fvxegyhltayjzl";
+	$pass = "c03c8ff4e52989e763d33295723382648271d60418e0b8517f7d2c9cbfb30da3";
 	$port = "5432";
-	$dbname = "d7gagsg14f5tng";
+	$dbname = "d18vd6kfetlpc2";
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
